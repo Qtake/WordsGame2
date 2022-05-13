a631250f88eac7710c17d8d3eb7ad36a5b8f0e34
Руслан Топольцев <grant99458@gmail.com>
2022-05-13 23:37:46 +0300

    Добавьте файлы проекта.

---
+++ WordGame2/Game.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordGame2
+{
+    internal class Game
+    {
+        public string ComposedWord { get; set; }
+        private readonly string _primaryWord;
+        private Stack<string> _usedWords;
+        
+        public Game(string primaryWord)
+        {
+            _primaryWord = primaryWord;
+            ComposedWord = string.Empty;
+            _usedWords = new Stack<string>();
+            _usedWords.Push(primaryWord);
+        }
+
+        public static bool CheckPrimaryWord(string primaryWord)
+        {
+            if ((primaryWord?.Length is < 8 or > 30) || primaryWord == string.Empty)
+            {
+                Console.WriteLine(Messages.WordLengthError);
+                return false;
+            }
+
+            return CheckCharacters(primaryWord);
+        }
+
+        public static bool CheckComposedWord(string composedWord)
+        {
+            if (composedWord == string.Empty)
+            {
+                Console.WriteLine(Messages.Lose);
+                return false;
+            }
+
+            return CheckCharacters(composedWord);
+        }
+
+        public static bool CheckCharacters(string word)
+        {
+            if (!Regex.IsMatch(word, Messages.LettersRegex))
+            {
+                Console.WriteLine(Messages.WordCharactersError);
+                return false;
+            }
+
+            return true;
+        }
+
+        private static IEnumerable<(char Letter, int Count)> CreateLettersGroup(string word)
+        {
+            return word
+                .GroupBy(c => c)
+                .Select(g => (g.Key, g.Count()));
+        }
+
+        public bool MatchLetters()
+        {
+            var primaryWordLetters = CreateLettersGroup(_primaryWord);
+            var composedWordLetters = CreateLettersGroup(ComposedWord);
+
+            foreach (var c1 in primaryWordLetters)
+            {
+                foreach (var c2 in composedWordLetters)
+                {
+                    if ((c1.Letter == c2.Letter && c1.Count != c2.Count))
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
+        }
+    }
+}
---
+++ WordGame2/Language.cs
@@ -0,0 +1,60 @@
+﻿using System.Globalization;
+using WordsGame.Languages;
+
+namespace WordGame2
+{
+    internal class Language
+    {
+        private readonly Dictionary<string, string> _languages = new Dictionary<string, string>()
+        {
+            { "English", "en-US" },
+            { "Русский", "ru-RU" }
+        };
+
+        private string _key;
+        private int _index;
+
+        public Language()
+        {
+            _key = string.Empty;
+            _index = 0;
+        }
+
+        public void CreateMenu()
+        {
+            string[] menuElements = _languages.Keys.ToArray();
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                PrintMenuElements(menuElements);
+                switch (Console.ReadKey(true).Key)
+                {
+                    case ConsoleKey.UpArrow:
+                        _index--;
+                        break;
+                    case ConsoleKey.DownArrow:
+                        _index++;
+                        break;
+                    case ConsoleKey.Enter:
+                        Console.CursorVisible = true;
+                        _key = menuElements[_index];
+                        Thread.CurrentThread.CurrentUICulture = CultureInfo.GetCultureInfo(_languages[_key]);
+                        return;
+                }
+                _index = (_index + menuElements.Length) % menuElements.Length;
+            }
+        }
+
+        private void PrintMenuElements(string[] menuElements)
+        {
+            Console.Clear();
+            Console.WriteLine(Messages.SelectLanguage);
+
+            for (int i = 0; i < menuElements.Length; i++)
+            {
+                Console.WriteLine("{0} {1}", menuElements[i], i == _index ? "<<--" : "");
+            }
+        }
+    }
+}
